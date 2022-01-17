12b8710e891255f229d8944757d0052ebbacf657
Danny <danny.affiliatemarketing@gmail.com>
2022-01-17 14:55:20 -0300

    .

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-//https://www.youtube.com/watch?v=ACP61O9f618&t=760s
-const container = document.getElementById('container')
+const container = document.querySelector('.container')
+const btnBlack = document.createElement('button')
+const btnRgb = document.createElement('button')
+const btnGray = document.createElement('button')
+const btnSize = document.createElement('button')
+const section = document.querySelector('.section');
+const buttonsContainer = document.querySelector('.buttons');
 
-function creatDivs(num) {
-    for(let i = 0;i < (num*num); i++) {
-        const div = document.createElement('div') 
-        container.appendChild(div).classList.add('box')
+
+function creatDivs(col , rows) {
+    for(let i = 0;i < (col * rows); i++) {
+        const div = document.createElement('div') ;
+        div.style.border = '1px solid red';
+        container.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+        container.appendChild(div).classList.add('box');
     }
 }
-creatDivs(16)
+creatDivs(16,16)
