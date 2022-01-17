10f3aed17fb48c39ca6155f2fa29cb745d2d6155
Danny <danny.affiliatemarketing@gmail.com>
2022-01-17 17:39:35 -0300

    .

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const grid = document.querySelector(".gridContainer");
 const userInput = document.getElementById("quantity");
 const resetButton = document.querySelector(".reset");
 
+// loop through to make grid dynamically
 createGrid = () => {
   for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
@@ -10,16 +11,21 @@ createGrid = () => {
   }
 };
 
+// allow for user input `${onput}`
 updateGrid = () => {
   grid.innerHTML = "";
   grid.style.setProperty(
     "grid-template-columns",
     `repeat(${userInput.value}, 2fr)`
   );
+  
+  // set grid styles
   grid.style.setProperty(
     "grid-template-rows",
     `repeat(${userInput.value}, 2fr)`
   );
+
+  //loop through using user input values
   for (let i = 0; i < userInput.value * userInput.value; i++) {
     const div = document.createElement("div");
     div.classList.add("square");
@@ -28,13 +34,16 @@ updateGrid = () => {
   console.log(userInput.value);
 };
 
+// add event listener for mouseover event
 const square = document.querySelector("div");
 square.addEventListener("mouseover", function(event) {
   event.target.classList.replace("square", "color");
 });
 
+// allow user to update grid
 userInput.addEventListener("change", updateGrid);
 
+// clear all when button is clicked to reset
 resetButton.addEventListener("click", function() {
   grid.innerHTML = "";
   userInput.value = "";
